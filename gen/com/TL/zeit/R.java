1e0401652db1ded49bdee398a509cb4f83f1bf0c
troyliu0105 <troyliu0105@outlook.com>
2014-09-28 21:38:23 +0800

    完成了SDK集成

--- gen/com/TL/zeit/R.java
+++ gen/com/TL/zeit/R.java
@@ -15,49 +15,195 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int com_sina_weibo_sdk_blue=0x7f080000;
+        public static final int com_sina_weibo_sdk_loginview_text_color=0x7f080001;
+    }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Example customization of dimensions originally defined in res/values/dimens.xml
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f050004;
+        public static final int activity_vertical_margin=0x7f050005;
+        public static final int com_sina_weibo_sdk_loginview_compound_drawable_padding=0x7f05000c;
+        public static final int com_sina_weibo_sdk_loginview_padding=0x7f050007;
+        public static final int com_sina_weibo_sdk_loginview_padding_bottom=0x7f05000b;
+        public static final int com_sina_weibo_sdk_loginview_padding_left=0x7f050008;
+        public static final int com_sina_weibo_sdk_loginview_padding_right=0x7f050009;
+        public static final int com_sina_weibo_sdk_loginview_padding_top=0x7f05000a;
+        public static final int com_sina_weibo_sdk_loginview_text_size=0x7f050006;
+        public static final int weibosdk_dialog_bottom_margin=0x7f050003;
+        /**  Default screen margins, per the Android Design guidelines. 
+         */
+        public static final int weibosdk_dialog_left_margin=0x7f050000;
+        public static final int weibosdk_dialog_right_margin=0x7f050002;
+        public static final int weibosdk_dialog_top_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int guide_background=0x7f020000;
-        public static final int guide_background_arbic=0x7f020001;
-        public static final int guide_background_china=0x7f020002;
-        public static final int guide_background_english=0x7f020003;
-        public static final int guide_background_france=0x7f020004;
-        public static final int guide_background_german=0x7f020005;
-        public static final int guide_background_japan=0x7f020006;
-        public static final int guide_background_klingon=0x7f020007;
-        public static final int guide_btn_default=0x7f020008;
-        public static final int guide_btn_press=0x7f020009;
-        public static final int ic_launcher=0x7f02000a;
-        public static final int login_background=0x7f02000b;
-        public static final int logo=0x7f02000c;
-        public static final int startpage=0x7f02000d;
+        public static final int com_sina_weibo_sdk_button_blue=0x7f020000;
+        public static final int com_sina_weibo_sdk_button_grey=0x7f020001;
+        public static final int com_sina_weibo_sdk_login_button_with_account_text=0x7f020002;
+        public static final int com_sina_weibo_sdk_login_button_with_frame_logo=0x7f020003;
+        public static final int com_sina_weibo_sdk_login_button_with_original_logo=0x7f020004;
+        public static final int guide_background=0x7f020005;
+        public static final int guide_background_arbic=0x7f020006;
+        public static final int guide_background_china=0x7f020007;
+        public static final int guide_background_english=0x7f020008;
+        public static final int guide_background_france=0x7f020009;
+        public static final int guide_background_german=0x7f02000a;
+        public static final int guide_background_japan=0x7f02000b;
+        public static final int guide_background_klingon=0x7f02000c;
+        public static final int guide_btn_default=0x7f02000d;
+        public static final int guide_btn_press=0x7f02000e;
+        public static final int ic_com_sina_weibo_sdk_button_blue_focused=0x7f02000f;
+        public static final int ic_com_sina_weibo_sdk_button_blue_normal=0x7f020010;
+        public static final int ic_com_sina_weibo_sdk_button_blue_pressed=0x7f020011;
+        public static final int ic_com_sina_weibo_sdk_button_grey_focused=0x7f020012;
+        public static final int ic_com_sina_weibo_sdk_button_grey_normal=0x7f020013;
+        public static final int ic_com_sina_weibo_sdk_button_grey_pressed=0x7f020014;
+        public static final int ic_com_sina_weibo_sdk_login_button_with_frame_logo_focused=0x7f020015;
+        public static final int ic_com_sina_weibo_sdk_login_button_with_frame_logo_normal=0x7f020016;
+        public static final int ic_com_sina_weibo_sdk_login_button_with_frame_logo_pressed=0x7f020017;
+        public static final int ic_com_sina_weibo_sdk_login_with_account_text_focused=0x7f020018;
+        public static final int ic_com_sina_weibo_sdk_login_with_account_text_normal=0x7f020019;
+        public static final int ic_com_sina_weibo_sdk_login_with_account_text_pressed=0x7f02001a;
+        public static final int ic_com_sina_weibo_sdk_login_with_text=0x7f02001b;
+        public static final int ic_com_sina_weibo_sdk_logo=0x7f02001c;
+        public static final int ic_launcher=0x7f02001d;
+        public static final int login_background=0x7f02001e;
+        public static final int logo=0x7f02001f;
+        public static final int startpage=0x7f020020;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int guide_background=0x7f090000;
-        public static final int guide_loginButton=0x7f090002;
-        public static final int guide_picture=0x7f090001;
+        public static final int action_settings=0x7f0a0003;
+        public static final int guide_background=0x7f0a0000;
+        public static final int guide_loginButton=0x7f0a0002;
+        public static final int guide_picture=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_guide=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int com_sina_weibo_sdk_login=0x7f060004;
+        public static final int com_sina_weibo_sdk_login_with_weibo_account=0x7f060003;
+        public static final int com_sina_weibo_sdk_logout=0x7f060005;
         public static final int hello_world=0x7f060001;
+        public static final int weibosdk_demo_access_token_is_empty=0x7f06005a;
+        public static final int weibosdk_demo_app_url=0x7f06003a;
+        public static final int weibosdk_demo_cancel_download_weibo=0x7f06002a;
+        public static final int weibosdk_demo_code_text_hint=0x7f060014;
+        public static final int weibosdk_demo_feature_login_logout=0x7f060008;
+        /**  功能列表 
+         */
+        public static final int weibosdk_demo_feature_oauth=0x7f060006;
+        public static final int weibosdk_demo_feature_open_api=0x7f060009;
+        public static final int weibosdk_demo_feature_share=0x7f060007;
+        public static final int weibosdk_demo_get_comment=0x7f06005c;
+        public static final int weibosdk_demo_get_group=0x7f06005d;
+        public static final int weibosdk_demo_get_home_mblog=0x7f06005b;
+        public static final int weibosdk_demo_has_installed_weibo=0x7f060028;
+        public static final int weibosdk_demo_has_not_installed_weibo=0x7f060029;
+        public static final int weibosdk_demo_is_installed_weibo=0x7f060024;
+        /**  分享微博入口界面 
+         */
+        public static final int weibosdk_demo_is_support_sdk=0x7f06001f;
+        /**  登录/注销按钮 
+         */
+        public static final int weibosdk_demo_login_hint=0x7f06004d;
+        public static final int weibosdk_demo_login_with_text_hint=0x7f06004e;
+        public static final int weibosdk_demo_logout=0x7f060050;
+        public static final int weibosdk_demo_logout_failed=0x7f060052;
+        public static final int weibosdk_demo_logout_failed_1=0x7f060053;
+        public static final int weibosdk_demo_logout_hint=0x7f06004f;
+        public static final int weibosdk_demo_logout_success=0x7f060051;
+        public static final int weibosdk_demo_not_support_api_hint=0x7f060027;
+        public static final int weibosdk_demo_obtain_client_abstract_info_hint=0x7f060023;
+        public static final int weibosdk_demo_obtain_token_hint=0x7f06000f;
+        public static final int weibosdk_demo_obtain_token_via_code=0x7f06000c;
+        public static final int weibosdk_demo_obtain_token_via_code_hint=0x7f060011;
+        public static final int weibosdk_demo_obtain_token_via_signature=0x7f06000b;
+        /**  获取 Token 
+         */
+        public static final int weibosdk_demo_obtain_token_via_sso=0x7f06000a;
+        public static final int weibosdk_demo_obtain_token_via_sso_hint=0x7f060010;
+        public static final int weibosdk_demo_openapi_invite=0x7f060057;
+        public static final int weibosdk_demo_openapi_invite_failed=0x7f060059;
+        public static final int weibosdk_demo_openapi_invite_success=0x7f060058;
+        public static final int weibosdk_demo_openapi_invite_uid_hint=0x7f060056;
+        public static final int weibosdk_demo_openapi_revoke_auth_failed=0x7f060055;
+        /**  OpenAPI 
+         */
+        public static final int weibosdk_demo_openapi_revoke_auth_success=0x7f060054;
+        public static final int weibosdk_demo_register_app_to_weibo=0x7f060020;
+        public static final int weibosdk_demo_register_app_to_weibo_hint=0x7f060022;
+        public static final int weibosdk_demo_share_default_text=0x7f06002e;
+        public static final int weibosdk_demo_share_from_weibo_title=0x7f06002d;
+        public static final int weibosdk_demo_share_image_title=0x7f060031;
+        public static final int weibosdk_demo_share_music_demo=0x7f060045;
+        public static final int weibosdk_demo_share_music_desc=0x7f060037;
+        public static final int weibosdk_demo_share_music_template=0x7f060041;
+        public static final int weibosdk_demo_share_music_title=0x7f060033;
+        /**  分享示例模板 
+         */
+        public static final int weibosdk_demo_share_text_template=0x7f06003f;
+        public static final int weibosdk_demo_share_text_title=0x7f060030;
+        public static final int weibosdk_demo_share_to=0x7f06002f;
+        public static final int weibosdk_demo_share_to_weibo=0x7f060021;
+        /**  分享微博主界面 
+         */
+        public static final int weibosdk_demo_share_to_weibo_title=0x7f06002c;
+        public static final int weibosdk_demo_share_video_demo=0x7f060046;
+        public static final int weibosdk_demo_share_video_desc=0x7f060038;
+        public static final int weibosdk_demo_share_video_template=0x7f060042;
+        public static final int weibosdk_demo_share_video_title=0x7f060034;
+        public static final int weibosdk_demo_share_voice_demo=0x7f060047;
+        public static final int weibosdk_demo_share_voice_desc=0x7f060039;
+        public static final int weibosdk_demo_share_voice_template=0x7f060043;
+        public static final int weibosdk_demo_share_voice_title=0x7f060035;
+        public static final int weibosdk_demo_share_webpage_demo=0x7f060044;
+        /**  分享所用的测试数据 
+         */
+        public static final int weibosdk_demo_share_webpage_desc=0x7f060036;
+        public static final int weibosdk_demo_share_webpage_template=0x7f060040;
+        public static final int weibosdk_demo_share_webpage_title=0x7f060032;
+        public static final int weibosdk_demo_step_to_obtain_code=0x7f060012;
+        public static final int weibosdk_demo_step_to_obtain_token=0x7f060013;
+        public static final int weibosdk_demo_support_api_level=0x7f060025;
+        public static final int weibosdk_demo_support_api_level_hint=0x7f060026;
+        public static final int weibosdk_demo_test_music_url=0x7f06003c;
+        public static final int weibosdk_demo_test_video_url=0x7f06003d;
+        /**  NOTE: Voice 的 URL 与 Music 一样 
+         */
+        public static final int weibosdk_demo_test_voice_url=0x7f06003e;
+        public static final int weibosdk_demo_test_webpage_url=0x7f06003b;
+        public static final int weibosdk_demo_toast_auth_canceled=0x7f06001a;
+        public static final int weibosdk_demo_toast_auth_failed=0x7f060019;
+        public static final int weibosdk_demo_toast_auth_success=0x7f060018;
+        public static final int weibosdk_demo_toast_obtain_code_failed=0x7f06001c;
+        public static final int weibosdk_demo_toast_obtain_code_success=0x7f06001b;
+        public static final int weibosdk_demo_toast_obtain_token_failed=0x7f06001e;
+        public static final int weibosdk_demo_toast_obtain_token_success=0x7f06001d;
+        public static final int weibosdk_demo_toast_register_app_to_weibo=0x7f06002b;
+        public static final int weibosdk_demo_toast_share_canceled=0x7f06004a;
+        public static final int weibosdk_demo_toast_share_failed=0x7f060049;
+        public static final int weibosdk_demo_toast_share_response_args_failed=0x7f06004c;
+        public static final int weibosdk_demo_toast_share_response_args_success=0x7f06004b;
+        /**  分享成功失败的 Toast 
+         */
+        public static final int weibosdk_demo_toast_share_success=0x7f060048;
+        public static final int weibosdk_demo_token_has_existed=0x7f06000e;
+        public static final int weibosdk_demo_token_info=0x7f06000d;
+        public static final int weibosdk_demo_token_text_hint=0x7f060015;
+        public static final int weibosdk_demo_token_to_string_format=0x7f060016;
+        public static final int weibosdk_demo_token_to_string_format_1=0x7f060017;
     }
     public static final class style {
         /** 
@@ -86,5 +232,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int com_sina_weibo_sdk_loginview_default_style=0x7f070002;
+        public static final int com_sina_weibo_sdk_loginview_silver_style=0x7f070003;
     }
 }
