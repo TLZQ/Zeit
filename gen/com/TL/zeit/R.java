5beae45478b5071ff67356e19b7b437489944f73
troyliu0105 <troyliu0105@outlook.com>
2014-09-29 15:22:43 +0800

    text

--- gen/com/TL/zeit/R.java
+++ gen/com/TL/zeit/R.java
@@ -80,9 +80,9 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f0a0003;
-        public static final int guide_background=0x7f0a0001;
+        public static final int guide_background=0x7f0a0000;
         public static final int guide_loginButton=0x7f0a0002;
-        public static final int guide_picture=0x7f0a0000;
+        public static final int guide_picture=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_guide=0x7f030000;
